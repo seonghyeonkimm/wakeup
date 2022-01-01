1a22e73dc788718f00488e1d557c969aaf6e2912
demian <demian@kcd.co.kr>
2022-01-01 23:22:24 +0900

    Add index.css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,43 @@
+import {
+  Container,
+  Text,
+  Box,
+  Progress,
+  Button,
+} from "@chakra-ui/react";
+
+
+import styles from '~/styles/index.css';
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container>
+      <Box p={4} textAlign="center">
+        <Text as="h1" fontSize="4xl" fontWeight="bold">
+          WakeUp
+        </Text>
+      </Box>
+      <Box mb={2}>
+        <Text fontSize="xl">
+          2022년의 10%가 지났어요.
+        </Text>
+      </Box>
+      <Progress
+        hasStripe
+        isAnimated
+        size="lg"
+        max={100}
+        value={10}
+      />
+      <Box textAlign="center" mt={8}>
+        <Button size="lg" colorScheme="blue">
+          WakeUp 알림 받기
+        </Button>
+      </Box>
+    </Container>
   );
 }
