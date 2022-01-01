2716071941879e973b1ea199110f8bf445df8268
demian <demian@kcd.co.kr>
2022-01-01 23:32:29 +0900

    Add getTodayPercentage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,14 +6,24 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-
 import styles from '~/styles/index.css';
+
+function getTodayPercentage() {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = now.getTime() - start.getTime();
+  const day = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const result = Math.floor((day / 365) * 100);
+  return result;
+}
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function Index() {
+  const todayPercentage = getTodayPercentage();
+
   return (
     <Container>
       <Box p={4} textAlign="center">
@@ -23,7 +33,7 @@ export default function Index() {
       </Box>
       <Box mb={2}>
         <Text fontSize="xl">
-          2022년의 10%가 지났어요.
+          2022년의 {todayPercentage}%가 지났어요.
         </Text>
       </Box>
       <Progress
@@ -31,7 +41,7 @@ export default function Index() {
         isAnimated
         size="lg"
         max={100}
-        value={10}
+        value={todayPercentage}
       />
       <Box textAlign="center" mt={8}>
         <Button size="lg" colorScheme="blue">
